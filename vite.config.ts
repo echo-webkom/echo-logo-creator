88b886bc8f96238125c8f851b24be8e4f46f8625
Ole Magnus Fon Johnsen <me@omfj.no>
2025-07-05 22:06:09 +0200

    Use Cloudflare for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwind()],
+  plugins: [react(), tailwind(), cloudflare()],
 });
